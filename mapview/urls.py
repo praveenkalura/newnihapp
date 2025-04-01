105348616f9a4b7796bb37ec8f57a8282c356d89
PRAVEEN KALURA <praveenkalura@gmail.com>
2025-04-01 11:31:54 +0530

    Update urls.py

--- mapview/urls.py
+++ mapview/urls.py
@@ -1,9 +1,20 @@
 from django.urls import path
 from . import views
+from django.urls import path, include
+from rest_framework.routers import DefaultRouter
+from .views import IndiaStateViewSet
 
 urlpatterns = [
     path('', views.home, name='home'),
     path('geojson/states/', views.states_geojson, name='states_geojson'),
     path('geojson/nih_centers/', views.centers_geojson, name='centers_geojson'),
     path('get_route/', views.get_route, name='get_route'),
+]
+
+
+router = DefaultRouter()
+router.register(r'india-states', IndiaStateViewSet)
+
+urlpatterns = [
+    path("api/", include(router.urls)),
 ]
