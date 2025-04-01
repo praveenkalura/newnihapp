6509373ac4626c014601efb5272e65bc061dc06c
PRAVEEN KALURA <praveenkalura@gmail.com>
2025-04-01 15:41:32 +0530

    Update views.py

--- mapview/views.py
+++ mapview/views.py
@@ -14,13 +14,3 @@ def states_geojson(request):
 def centers_geojson(request):
     data = serialize('geojson', NIHCenter.objects.all(), geometry_field='geom', fields=['name'])
     return JsonResponse(data, safe=False)
-
-def get_route(request):
-    coords = request.GET.getlist('coords[]')
-    client = openrouteservice.Client(key='YOUR_API_KEY')
-    route = client.directions(
-        coordinates=[[float(coords[0]), float(coords[1])], [float(coords[2]), float(coords[3])]],
-        profile='driving-car',
-        format='geojson'
-    )
-    return JsonResponse(route)
