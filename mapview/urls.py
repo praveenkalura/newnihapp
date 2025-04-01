fb1d7c9be20f591f7fb0aed78814ae3fb138417c
PRAVEEN KALURA <praveenkalura@gmail.com>
2025-04-01 16:34:45 +0530

    Delete mapview/urls.py

--- mapview/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('', views.home, name='home'),
-    path('geojson/states/', views.states_geojson, name='states_geojson'),
-    path('geojson/nih_centers/', views.centers_geojson, name='centers_geojson'),
-    path('get_route/', views.get_route, name='get_route'),
-]
